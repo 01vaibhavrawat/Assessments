2dfff0ea34f82f694c682d765453729c575d73b4
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-07-02 17:29:09 +0530

    first day of work on about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,21 @@
 const About = (props) => {
+	return(
+		<div id="about">
+<div id="mission">
+<h3 id="card-title">hello world</h3>
+hello world sjkdfl lksjdf sadlfj  sdfjklasdj fkl aslkdjfklas 
+dfklj sdklfj ksdj fklj sadklfj lskdj f j asjdfl a sdkljf s jf asjdf sdj flsj
+adfklj sdfjlsjd fl afj sdfj sdlfjs dfsd fsl df sdfjsdlfj  slkdjf
+</div>
+<div id="team">
+<h3 id="card-title">hello world</h3>
+hello world sjkdfl lksjdf sadlfj  sdfjklasdj fkl aslkdjfklas 
+dfklj sdklfj ksdj fklj sadklfj lskdj f j asjdfl a sdkljf s jf asjdf sdj flsj
+adfklj sdfjlsjd fl afj sdfj sdlfjs dfsd fsl df sdfjsdlfj  slkdjf
 
+</div>
+</div>
+)
 
 }
 
