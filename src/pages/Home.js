e7c54bb8874c10df031cb116029ca3eb496365f7
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-07-23 11:20:39 +0530

    login form created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
 		<div>
 		<div>Hello World! I need a one dance</div>
 		<ul>
-		<li><Link to="/">Home</Link></li>
 		<li><Link to="/home">Home</Link></li>
+		<li><Link to="/login">Login</Link></li>
 		<li><Link to="/story">Story</Link></li>
 		<li><Link to="/about">About</Link></li>
 		<li><Link to="/asessment">Asessment</Link></li>
--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,12 +1,57 @@
-import {Link} from 'react-router-dom'
+import {
+	FormGroup,
+	Form,
+	Label,
+	Input,
+	Button
+} from 'reactstrap'
 
 const LoginForm = (props) => {
 
 
 return(
+	<div id="login-body">
+	<div id="login-box">
 
-	<p>are you because of this code or not</p>)}
-
+	<Form inline id="login-form">
+  <FormGroup>
+    <Label
+      for="exampleEmail"
+      hidden
+    >
+      Email
+    </Label>
+    <Input
+      id="exampleEmail"
+      name="email"
+      placeholder="Email"
+      type="email"
+    />
+  </FormGroup>
+  {' '}
+  <FormGroup>
+    <Label
+      for="examplePassword"
+      hidden
+    >
+      Password
+    </Label>
+    <Input
+      id="examplePassword"
+      name="password"
+      placeholder="Password"
+      type="password"
+    />
+  </FormGroup>
+  {' '}
+  <Button id="login-submit">
+    Submit
+  </Button>
+  <div id="forgot">Forgot password?</div> 
+</Form>
+</div>
+</div>
+)
+}
 	
-
 export default LoginForm
