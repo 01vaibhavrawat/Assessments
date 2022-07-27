9ae9f5e030e0cad4e55bd926dcefdb0bed1bb464
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-07-27 18:15:42 +0530

    let's go finally found the solution to make reactstrap navbar work

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import {useState} from 'react'
+import {Link} from 'react-router-dom';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap'
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
-const NavCom = (props) => {
-  const [isOpen, setIsOpen] = React.useState(false)
-  return (
-   <nav>hello world</nav>
-    )
-}
-export default NavCom
+
+const NavCom = (args) => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+      return (
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink tag={Link} to="/plantInfo">Plant Info</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu end>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+  export default NavCom
