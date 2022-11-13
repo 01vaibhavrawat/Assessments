ae10ceec8f6479fe87e849c6769ade030f7560df
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-11-13 15:33:33 +0530

    fixed bug

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -17,7 +17,7 @@ const NavCom = (args) => {
 
       return (
           <Navbar id="navbar" expand="md" variant="light">
-            <NavbarBrand id="logo" href="/home">CruelMe.com</NavbarBrand>
+            <NavbarBrand id="logo"><NavLink tag={Link} to="/home">CruelMe.com</NavLink></NavbarBrand>
             <NavbarToggler id="toggler" onClick={toggle}><img id="nav-icon" src={require('../images/menu.png')} /></NavbarToggler>
             <Collapse isOpen={isOpen} navbar>
               <Nav className="ml-auto" navbar>
